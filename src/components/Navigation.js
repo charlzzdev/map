d8e3872da0fe42c0378ef4931e848e9c84628bf6
Charles <ellerkaresz@gmail.com>
2020-02-28 19:34:40 +0100

    close dropdown on clicking outside of it

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Collapsible from './Collapsible';
 
 const Navigation = ({ setTiles }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  useEffect(() => {
+    const map = document.getElementById('map');
+    map.addEventListener('click', () => setDropdownOpen(false));
+  }, []);
 
   return (
     <nav className="main-nav">
