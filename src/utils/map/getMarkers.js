507ea77b5cf5ad1cb6bc4ae2df45b730d9df13a9
Charles <ellerkaresz@gmail.com>
2020-03-05 16:46:25 +0100

    put marker click event in separate file

--- src/utils/map/getMarkers.js
+++ src/utils/map/getMarkers.js
@@ -4,7 +4,7 @@ import 'firebase/firestore';
 import 'firebase/storage';
 import 'firebase/auth';
 
-import extraImageUpload from '../form/extraImageUpload';
+import onMarkerClick from './onMarkerClick';
 
 const getMarkers = (map, tiles) => {
   const markers = [];
@@ -46,58 +46,11 @@ const getMarkers = (map, tiles) => {
           <div class="marker-images" id="${latlng}"></div>
         `, { minWidth: 300 });
 
-        marker.on('click', () => {
-          if (!marker.getPopup().isOpen()) return;
-
-          const sortSelect = document.getElementById(`sort-${latlng}`);
-          sortSelect.addEventListener('change', () => {
-            const images = document.querySelectorAll('.marker-images *');
-            images.forEach(image => {
-              image.style.order = -image.style.order;
-            });
-          });
-
-          if (firebase.auth().currentUser) {
-            const deleteMarkerBtn = document.querySelector(`.danger-btn.delete-marker`);
-            deleteMarkerBtn.addEventListener('click', () => {
-              firebase.firestore()
-                .collection(`tiles/${tiles}/markers`)
-                .doc(doc.id)
-                .delete();
-
-              firebase.storage().ref()
-                .child(`images/${tiles}/${latlng}`)
-                .listAll()
-                .then(data => {
-                  data.items.forEach(item => item.delete());
-                });
-            });
-          }
-
-          firebase.storage().ref().child(`images/${tiles}/${latlng}`)
-            .listAll()
-            .then(data => {
-              const markerImages = document.getElementById(latlng);
-              const uploadForm = markerImages.parentElement.querySelector('form');
-              extraImageUpload(uploadForm, tiles, latlng);
-
-              data.items.forEach(item => {
-                item.getDownloadURL()
-                  .then(url => {
-                    const fileName = url.split(encodeURIComponent(`${latlng}/`))[1];
-                    const title = fileName.split('_')[1].split('.')[0];
-                    const date = fileName.split('_')[0];
-                    const dateWithoutDots = date.split('.').join('') * -1;
-
-                    markerImages.innerHTML += `
-                      <h2 style="order: ${dateWithoutDots}; margin: 0.75rem 0;">
-                        ${date} ${title && '-'} ${title}
-                      </h2>
-                      <img src="${url}" style="order: ${dateWithoutDots};">
-                    `;
-                  });
-              });
-            });
+        onMarkerClick({
+          marker,
+          tiles,
+          latlng,
+          docId: doc.id
         });
       });
     });
---
+++ src/utils/map/onMarkerClick.js
@@ -0,0 +1,63 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+import extraImageUpload from '../form/extraImageUpload';
+
+const onMarkerClick = ({ marker, tiles, latlng, docId }) => {
+  marker.on('click', () => {
+    if (!marker.getPopup().isOpen()) return;
+
+    const sortSelect = document.getElementById(`sort-${latlng}`);
+    sortSelect.addEventListener('change', () => {
+      const images = document.querySelectorAll('.marker-images *');
+      images.forEach(image => {
+        image.style.order = -image.style.order;
+      });
+    });
+
+    if (firebase.auth().currentUser) {
+      const deleteMarkerBtn = document.querySelector(`.danger-btn.delete-marker`);
+      deleteMarkerBtn.addEventListener('click', () => {
+        firebase.firestore()
+          .collection(`tiles/${tiles}/markers`)
+          .doc(docId)
+          .delete();
+
+        firebase.storage().ref()
+          .child(`images/${tiles}/${latlng}`)
+          .listAll()
+          .then(data => {
+            data.items.forEach(item => item.delete());
+          });
+      });
+    }
+
+    firebase.storage().ref().child(`images/${tiles}/${latlng}`)
+      .listAll()
+      .then(data => {
+        const markerImages = document.getElementById(latlng);
+        const uploadForm = markerImages.parentElement.querySelector('form');
+        extraImageUpload(uploadForm, tiles, latlng);
+
+        data.items.forEach(item => {
+          item.getDownloadURL()
+            .then(url => {
+              const fileName = url.split(encodeURIComponent(`${latlng}/`))[1];
+              const title = fileName.split('_')[1].split('.')[0];
+              const date = fileName.split('_')[0];
+              const dateWithoutDots = date.split('.').join('') * -1;
+
+              markerImages.innerHTML += `
+                <h2 style="order: ${dateWithoutDots}; margin: 0.75rem 0;">
+                  ${date} ${title && '-'} ${title}
+                </h2>
+                <img src="${url}" style="order: ${dateWithoutDots};">
+              `;
+            });
+        });
+      });
+  });
+}
+
+export default onMarkerClick;
